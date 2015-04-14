4b96b871dc414727160b51a7a454c518382ea9aa
Vincent Deygas <Vincent.Deygas@rewardgateway.com>
2015-04-14 13:03:21 -0400

    Add value for value for performance.readdir-ahead volume property.

--- manifests/volume/property/data.pp
+++ manifests/volume/property/data.pp
@@ -295,6 +295,9 @@ class gluster::volume::property::data() {
 		# Number of pages that will be pre-fetched
 		'performance.read-ahead-page-count' => 'integer',	# default: 4
 
+		# enable/disable readdir-ahead translator in the volume.
+		'performance.readdir-ahead' => 'offon',	# default: off
+
 		# enable/disable meta-data caching translator in the volume.
 		'performance.stat-prefetch' => 'onoff',	# default: on
 
